5829a8fdd938bd77429a995469bc6683429fc055
Ilya Holtz <holtzilya2008@gmail.com>
2022-06-05 19:42:34 +0300

    switch version -  update BL phase 1
    (cherry picked from commit 93e353b7160abf40540cd2c41cbf5ec8ac452a99)

--- billing-app/billing-backend/src/services/order/order.service.ts
+++ billing-app/billing-backend/src/services/order/order.service.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateOrderDTO } from 'src/dto/request';
 import { SuccessResponseDTO } from 'src/dto/response';
+import { OrderStatus } from 'src/types';
 import { emulateAsyncProccess } from 'src/utils';
+import { HIGH_COST_BARRIER_USD } from './constants';
 
 @Injectable()
 export class OrderService {
   async update(order: UpdateOrderDTO): Promise<SuccessResponseDTO> {
     console.log(`${this.constructor.name}.update start`);
-    await emulateAsyncProccess('Update Order', order);
+    if (order.status >= OrderStatus.Final) {
+      await emulateAsyncProccess(
+        'validate if the customer is allowed for one-time payment',
+        order,
+      );
+      await emulateAsyncProccess(
+        'calculate up to date tax according to price',
+        order,
+      );
+    }
+    if (
+      (order.status === OrderStatus.Aprooved ||
+        order.status === OrderStatus.Paid) &&
+      order.totalCost >= HIGH_COST_BARRIER_USD
+    ) {
+      await emulateAsyncProccess(
+        'execute High cost order business logic',
+        order,
+      );
+    }
     return {
       success: true,
     };
   }
+
 }
