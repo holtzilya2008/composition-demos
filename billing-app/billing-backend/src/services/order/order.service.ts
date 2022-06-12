81fa803b35b5480ec1d3061d6641128227602d37
Ilya Holtz <holtzilya2008@gmail.com>
2022-06-12 23:09:14 +0300

    fixing typos

--- billing-app/billing-backend/src/services/order/order.service.ts
+++ billing-app/billing-backend/src/services/order/order.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateOrderDTO } from 'src/dto/request';
 import { SuccessResponseDTO } from 'src/dto/response';
-import { emulateAsyncProccess } from 'src/utils';
+import { emulateAsyncProcess } from 'src/utils';
 
 @Injectable()
 export class OrderService {
   async update(order: UpdateOrderDTO): Promise<SuccessResponseDTO> {
     console.log(`${this.constructor.name}.update start`);
-    await emulateAsyncProccess('Update Order', order);
+    await emulateAsyncProcess('Update Order', order);
     return {
       success: true,
     };
--- billing-app/billing-backend/src/utils/emulate-async-proccess.util.ts
+++ billing-app/billing-backend/src/utils/emulate-async-proccess.util.ts
@@ -1,14 +1,14 @@
 import { firstValueFrom, map, timer } from 'rxjs';
 
-export async function emulateAsyncProccess(
+export async function emulateAsyncProcess(
   message: string,
   target: any,
 ): Promise<void> {
   console.log(message);
-  console.log(`proccess started ... `);
+  console.log(`process started ... `);
   const observable = timer(100).pipe(
     map(() => {
-      console.log(`proccess completed successfully!!`);
+      console.log(`process completed successfully!!`);
       console.log(JSON.stringify(target, null, 2));
     }),
   );
