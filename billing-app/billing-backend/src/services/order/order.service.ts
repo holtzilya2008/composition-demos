3a94718ce5ad0eb925f2960783b87907352ecf7e
Ilya Holtz <holtzilya2008@gmail.com>
2022-06-12 23:10:18 +0300

    empty function

--- billing-app/billing-backend/src/services/order/order.service.ts
+++ billing-app/billing-backend/src/services/order/order.service.ts
@@ -6,8 +6,9 @@ import { emulateAsyncProcess } from 'src/utils';
 @Injectable()
 export class OrderService {
   async update(order: UpdateOrderDTO): Promise<SuccessResponseDTO> {
-    console.log(`${this.constructor.name}.update start`);
+
     await emulateAsyncProcess('Update Order', order);
+
     return {
       success: true,
     };
