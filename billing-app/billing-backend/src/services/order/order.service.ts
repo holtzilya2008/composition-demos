32dc302d599750b3faa76cbd25b6ce2f9a5de4f9
Ilya Holtz <holtzilya2008@gmail.com>
2022-06-12 23:29:20 +0300

    switch version - phase 1

--- billing-app/billing-backend/src/services/order/order.service.ts
+++ billing-app/billing-backend/src/services/order/order.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateOrderDTO } from 'src/dto/request';
 import { SuccessResponseDTO } from 'src/dto/response';
+import { OrderStatus } from 'src/types';
 import { emulateAsyncProcess } from 'src/utils';
+import { HIGH_COST_BARRIER_USD } from './constants';
 
 @Injectable()
 export class OrderService {
   async update(order: UpdateOrderDTO): Promise<SuccessResponseDTO> {
-
-    await emulateAsyncProcess('Update Order', order);
-
+    console.log(`${this.constructor.name}.update start`);
+    if (order.status >= OrderStatus.Final) {
+      await this.validateIfTheCustomerIsAllowedForOTP(order);
+      await this.calculateUpToDateTaxAccordingToPrice(order);
+    }
+    if (
+      (order.status === OrderStatus.Approved ||
+        order.status === OrderStatus.Paid) &&
+      order.totalCost >= HIGH_COST_BARRIER_USD
+    ) {
+      await this.executeHighCostOrderBL(order);
+    }
     return {
       success: true,
     };
+  }
+
+  private async validateIfTheCustomerIsAllowedForOTP(
+    order: UpdateOrderDTO,
+  ): Promise<void> {
+    await emulateAsyncProcess(
+      'validate if the customer is allowed for one-time payment',
+      order,
+    );
+  }
+
+  private async calculateUpToDateTaxAccordingToPrice(
+    order: UpdateOrderDTO,
+  ): Promise<void> {
+    await emulateAsyncProcess(
+      'calculate up to date tax according to price',
+      order,
+    );
+  }
+
+  private async executeHighCostOrderBL(order: UpdateOrderDTO): Promise<void> {
+    await emulateAsyncProcess('execute High cost order business logic', order);
   }
 }
